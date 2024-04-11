18a5a27476929df2ac163137f3b242ee5d706d02
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-11 20:01:10 +0530

    Adding our features file and start develop it.

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import LandingPage from "./landingPage";
+import Features from "./ourFeatures";
 
 const Home = () => {
-    return <LandingPage />;
+    return (
+        <>
+            <LandingPage />
+            <Features />
+        </>
+    );
 };
 
 export default Home;
