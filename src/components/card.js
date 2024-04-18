98ac4f59f97e08fa549c5f121c10e070d7ce28b0
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-18 16:38:59 +0530

    Adding cards in our explore section.

---
+++ src/components/card.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./card.css";
+
+const card = ({ data }) => {
+    return (
+        <div>
+            <div className="cards-container">
+                {data.map((value, index) => (
+                    <div className="card1" key={index} id={value.id}>
+                        <div className="card1-img">
+                            <img src={value.icon} alt="Icon1" />
+                        </div>
+                        <div className="card1-heading">
+                            <h2>{value.title}</h2>
+                        </div>
+                        <div className="card1-para">
+                            <p>{value.desc}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default card;
--- src/components/ourFeatures.js
+++ src/components/ourFeatures.js
@@ -1,10 +1,34 @@
 import React from "react";
-import './ourFeatures.css'
-
+import "./ourFeatures.css";
+import Card from "./card";
 import Icon1 from "../resource/service-icon-01.webp";
 import Icon2 from "../resource/service-icon-02.webp";
 import Icon3 from "../resource/service-icon-03.webp";
 import Icon4 from "../resource/service-icon-04.webp";
+const cardContent = [
+    {
+        icon: Icon1,
+        title: "Trading Engine",
+        desc: "An intelligent tool for automated trading, executing trades based on user-defined rules.",
+    },
+    {
+        id: "c2",
+        icon: Icon2,
+        title: "Strategy Bulding Wizard",
+        desc: "Revolutionizing trading by offering smart strategies without coding. Users can create and sell ideas.",
+    },
+    {
+        id: "c2",
+        icon: Icon3,
+        title: "Social Trading",
+        desc: "Choose trader strategies based on performance, replicate their actions in your portfolio, even without expertise.",
+    },
+    {
+        icon: Icon4,
+        title: "BackTesting",
+        desc: "Manage trading differences using three backtesting options, with the worst-case scenario.",
+    },
+];
 
 const Features = () => {
     return (
@@ -17,40 +41,7 @@ const Features = () => {
                     capital trading with revolutionary AI-powered capabilities.
                 </p>
             </div>
-            <div className="cards-container">
-                <div className="card1">
-                    <img src={Icon1} alt="Icon1" />
-                    <h2>Trading Engine</h2>
-                    <p>
-                        An intelligent tool for automated trading, executing
-                        trades based on user-defined rules.
-                    </p>
-                </div>
-                <div className="card2">
-                    <img src={Icon2} alt="Icon2" />
-                    <h2>Strategy Building Wizard</h2>
-                    <p>
-                        Revolutionizing trading by offering smart strategies
-                        without coding. Users can create and sell ideas.
-                    </p>
-                </div>
-                <div className="card3">
-                    <img src={Icon3} alt="Icon3" />
-                    <h2>Socail Trading</h2>
-                    <p>
-                        Choose trader strategies based on performance, replicate
-                        their actions in your portfolio, even without expertise.
-                    </p>
-                </div>
-                <div className="card4">
-                    <img src={Icon4} alt="Icon4" />
-                    <h2>BackTesting</h2>
-                    <p>
-                        Manage trading differences using three backtesting
-                        options, with the worst-case scenario.
-                    </p>
-                </div>
-            </div>
+            <Card data={cardContent} />
         </div>
     );
 };
