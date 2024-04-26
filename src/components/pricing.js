4e141fe13924f4ed2f6ed394afca966c490dd0fa
Abhishek Yadav <abhishekay442080@gmail.com>
2024-04-26 14:19:43 +0530

    Complete pricing section

--- src/components/pricing.js
+++ src/components/pricing.js
@@ -1,17 +1,102 @@
-import React from 'react'
-import './pricing.css';
-import PricingCard from './pricingCard';
+import React, { useState } from "react";
+import PricingCard from "./pricingCard";
 
-const pricing = () => {
-  return (
-    <div className='pricing-container'>
-        <div className='pricing-head'>
-            <h1>Our Pricing Plan</h1>
-            <p>Pricing That Adapts to Your Success</p>
+import price1 from "../resource/pr1.svg";
+import price2 from "../resource/pr2.svg";
+import price3 from "../resource/pr3.svg";
+
+const Pricing = () => {
+    const [priceData, setPriceData] = useState({
+        free: 0,
+        unlimited: 81,
+        limited: 48,
+    });
+
+    const pricingData = [
+        {
+            category: "Free",
+            logo: price1,
+            planPrice: priceData.free,
+            crossIndex: 4,
+            features: [
+                "50 Backtest Credit Points",
+                "5 Strategy Creation",
+                "No Basic + HNI & Retail Strategy Template Access",
+                "Reporting Console",
+                "No Strategy Deployment (Live & Forward)",
+                "No Portfolio Backtesting",
+            ],
+        },
+        {
+            category: "unlimited plan",
+            logo: price2,
+            planPrice: priceData.unlimited,
+            features: [
+                "1500 Backtest Credit Points",
+                "50 Strategy Creation",
+                "Basic + HNI & Retail Strategy Template Access",
+                "Reporting Console",
+                "20 Strategy Deployment (Live & Forward)",
+                "10 Strategy Portfolio Backtesting",
+            ],
+        },
+        {
+            category: "limited plan",
+            logo: price3,
+            planPrice: priceData.limited,
+            features: [
+                "500 Backtest Credit Points",
+                "25 Strategy Creation",
+                "Basic + HNI & Retail Strategy Template Access",
+                "Reporting Console",
+                "5 Strategy Deployment (Live & Forward)",
+                "2 Strategy Portfolio Backtesting",
+            ],
+        },
+    ];
+
+    const handleMonthly = () => {
+        setPriceData({
+            free: 0,
+            unlimited: 81,
+            limited: 48,
+        });
+    };
+    const handleQuarterly = () => {
+        setPriceData({
+            free: 0,
+            unlimited: 69,
+            limited: 41,
+        });
+    };
+    const handleYearly = () => {
+        setPriceData({
+            free: 0,
+            unlimited: 58,
+            limited: 34,
+        });
+    };
+
+    return (
+        <div className="pricing-container">
+            <div className="pricing-head">
+                <h1>Our Pricing Plan</h1>
+                <p>Pricing That Adapts to Your Success</p>
+            </div>
+            <div className="priceMenus">
+                <div className="priceMenuBtn">
+                    <button onClick={handleMonthly}>Monthly</button>
+                </div>
+                <div className="priceMenuBtn">
+                    <button onClick={handleQuarterly}>Quarterly</button>
+                </div>
+                <div className="priceMenuBtn">
+                    <button onClick={handleYearly}>Yearly</button>
+                </div>
+            </div>
+            <PricingCard data={pricingData} />
         </div>
-        <PricingCard />
-    </div>
-  )
-}
+    );
+};
 
-export default pricing
+export default Pricing;
--- src/components/pricingCard.js
+++ src/components/pricingCard.js
@@ -1,32 +1,49 @@
-import React from 'react';
-import price1 from '../resource/pr1.svg';
-
-const pricingCard = () => {
-  return (
-    <div>
-        <div className='pricingCard-container'>
-            <div className='pricingCard'>
-                <div className='plan-name'>
-                    <h1>FREE</h1>
-                </div>
-                <div className='pr-img'>
-                    <img src={price1} alt='Free' />
-                </div>
-                <div className='price&day'>
-                    <h1>₹</h1>
-                    <p>/day</p>
-                    <span></span>
-                </div>
-                <div className='plan-features'>
-                    
-                </div>
-                <div>
-                    <button>Get Started</button>
-                </div>
+import React from "react";
+import "./pricingCard.css";
+import { TiTick } from "react-icons/ti";
+import { ImCross } from "react-icons/im";
+const pricingCard = ({ data }) => {
+    return (
+        <div>
+            <div className="pricingCard-container">
+                {data.map((value, index) => (
+                    <div className="pricingCard" key={index}>
+                        <div className="plan-name">
+                            <h6>{value.category}</h6>
+                        </div>
+                        <div className="pr-img">
+                            <img src={value.logo} alt="Free" />
+                        </div>
+                        <div className="priceday">
+                            <h1>
+                                ₹{value.planPrice} <span>/ day</span>
+                            </h1>
+                        </div>
+                        <span className="line"></span>
+                        <div className="plan-features">
+                            {value?.features?.map((feature,cross) => {
+                                return (
+                                    <p>
+                                        {value.crossIndex == cross || value.crossIndex + 2 == cross + 1 ? (
+                                            <div>
+                                                <ImCross color="#007bff" size={12} />
+                                            </div>
+                                        ) : (
+                                            <TiTick color="#007bff" size={18} />
+                                        )}
+                                        {feature}
+                                    </p>
+                                );
+                            })}
+                        </div>
+                        <div className="price-btn">
+                            <button>Get Started</button>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default pricingCard
+export default pricingCard;
