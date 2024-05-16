aff6cd00865caea554bfaafb02bfcfb1243303c2
Abhishek Yadav <abhishekay442080@gmail.com>
2024-05-16 21:47:33 +0530

    adding files for login and signup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import Home from "./components/homePage";
 import Footer from "./components/footer";
 import axios from "axios";
 import { useEffect } from "react";
-
+import { Route,Routes } from "react-router-dom";
+import Login from "./components/Login/Login";
+import Signup from "./components/Signup/Signup";
 function App() {
     const getStatus = async () => {
         const resp = await axios.get("http://localhost:8000/testing");
@@ -17,11 +19,15 @@ function App() {
 
     return (
         <div>
-            <div id="scrollOnTop">
+            {/* <div id="scrollOnTop">
                 <Header />
-            </div>
-            <Home />
-            <Footer />
+            </div> */}
+            <Routes>
+                <Route path="/" element={<Home />}/>
+                <Route path="/login" element={<Login />}/>
+                <Route path="/signup" element={<Signup />}/>
+            </Routes>
+            {/* <Footer /> */}
         </div>
     );
 }
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -1,20 +1,26 @@
 import React from "react";
 import LandingPage from "./landingPage";
 import Features from "./ourFeatures";
-import Spectrum from './spectrum';
-import Pricing from './pricing';
+import Spectrum from "./spectrum";
+import Pricing from "./pricing";
 import Testonomial from "./testinomial";
 import Contact from "./contact";
+import Header from "./header";
+import Footer from "./footer";
 
 const Home = () => {
     return (
         <>
+            <div id="scrollOnTop">
+                <Header />
+            </div>
             <LandingPage />
             <Features />
             <Spectrum />
             <Pricing />
             <Testonomial />
             <Contact />
+            <Footer />
         </>
     );
 };
