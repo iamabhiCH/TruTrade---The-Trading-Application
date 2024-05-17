dc6226185ffabd37a4fa11799f9786b3f6e5e7f4
Abhishek Yadav <abhishekay442080@gmail.com>
2024-05-17 23:54:00 +0530

    the styling of the signup page is completed.

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Signup.css";
 import Slider from "react-slick";
 import EmailIcon from '@mui/icons-material/Email';
-
+import {Link} from 'react-router-dom'
 import SignupImg1 from "../../resource/signup1.svg";
 import SignupImg2 from "../../resource/signup2.svg";
 
@@ -45,13 +45,14 @@ const Signup = () => {
                 <div className="signup-content">
                     <h1>Welcome!</h1>
                     <p>
-                        Please enter your email to send OTP! or go back to Login
+                        Please enter your email to send OTP! or go back to <Link to='/login' className="login-page-switch">Login</Link> 
                     </p>
                     <form>
                         <div className="signup-form">
-                            <EmailIcon style={{ color: '#123591' }} />
-                            <input type="email" placeholder="Email" />
+                            <EmailIcon className="emailIcon" style={{ color: '#123591' }} />
+                            <input type="email" className="emailInput" placeholder="Email ID / Client ID" required/>
                         </div>
+                        <button>Send OTP</button>
                     </form>
                     <h6>Copyright © 2024 TruTrade. All rights reserved.</h6>
                 </div>
